d5d34a84398ab6b1a75b12ebbe93e3780a29766a
RishardTan <64884604+RishardTan@users.noreply.github.com>
2022-11-02 10:13:15 +0700

    Add files via upload

---
+++ resources/views/header.blade.php
@@ -0,0 +1,5 @@
+<div class="header" style="text-align: center; width: 100%">
+    <div class="text">
+        <h1  style="margin-top: 50px; margin-bottom: 50px; font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif">Welcome To The Multiplication Table</h1>
+    </div>
+</div>
